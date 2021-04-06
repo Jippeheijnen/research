1e93c87d434eed16ab3679f590b242c4c8b1c983
Jippe Heijnen <jippe.heijnen@student.hu.nl>
2021-04-06 12:27:15 +0200

    added untracked files woops

---
+++ devel/share/kobuki_msgs/cmake/kobuki_msgs-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(kobuki_msgs_MSG_INCLUDE_DIRS "/home/jippe/research/src/kobuki_msgs/msg;/home/jippe/research/devel/share/kobuki_msgs/msg")
+set(kobuki_msgs_MSG_DEPENDENCIES std_msgs;actionlib_msgs)

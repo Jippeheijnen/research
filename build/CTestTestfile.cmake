2223f8613ccf7734b9168e6219e8d1d1791938d5
Jippe Heijnen <jippe.heijnen@student.hu.nl>
2021-04-06 11:11:42 +0200

    added the initial workspace to vcs

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/jippe/research/src
+# Build directory: /home/jippe/research/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")

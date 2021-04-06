d4f7a2d7362995902b6d122f584949f1dd0ef59e
Jippe Heijnen <jippe.heijnen@student.hu.nl>
2021-04-06 11:17:23 +0200

    Added turtlebot packages

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,6 +8,9 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/jippe/research/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/jippe/research/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/jippe/research/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/jippe/research/build'
+export PYTHONPATH="/home/jippe/research/devel/lib/python2.7/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES='/home/jippe/research/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/jippe/research/src:$ROS_PACKAGE_PATH"
